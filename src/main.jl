43af0f449e59fd056a54fd477005db880fbffc9a
Kuba <kubawlas1@gmail.com>
2026-06-08 15:15:56 +0200

    progress

--- src/main.jl
+++ src/main.jl
@@ -1,13 +1,20 @@
-using Graphs
-import Graphs: pagerank as pagerank_lib
-using GraphRecipes, Plots
-include("./simplerank.jl")
+include("pagerank.jl");
+include("simplerank.jl");
+include("plotting.jl")
 using .simplerank
-pythonplot()
+using .pagerank
+using .plotting
+using Karnak
+using Arpack
+using LinearAlgebra
+using Graphs
+using Plots
+using Statistics
+import Graphs: pagerank as pagerank_lib
 
 bara_alb = barabasi_albert(20, 11, 5, is_directed=true)
 graf = DiGraph(Edge.([(1, 2), (2, 3), (3, 2), (3, 5), (4, 3), (6, 3), (4, 1), (6, 4), (5, 2), (4, 6)]))
-erdos = erdos_renyi(20, 0.2, is_directed=true)
+erdos = erdos_renyi(2000, 0.2, is_directed=true)
 simplerank.graph_simplerank(bara_alb, 0.85)
 
 a = 2
--- src/pagerank.jl
+++ src/pagerank.jl
@@ -4,6 +4,7 @@ include("./simplerank.jl")
 import .simplerank: matrix_A, power_iteration
 using Graphs
 using LinearAlgebra
+using SparseArrays
 
 function graph_pagerank(graph, d, e, ϵ)
     @assert 0.0 < d <= 1.0
@@ -12,7 +13,17 @@ function graph_pagerank(graph, d, e, ϵ)
     v[v.<ϵ] .= 0.0
     return any(v .< 0) ? -v : v
 end
+
+function graph_pagerank(graph, d, e, ϵ, ::Val{:delta_data})
+    @assert 0.0 < d <= 1.0
+    Â = matrix_Â(graph, e)
+    v, delta_v = pagerank_power_iter_with_mass_re(Â, d, e, ϵ, Val(:delta_data))
+    v[v.<ϵ] .= 0.0
+    return any(v .< 0) ? -v : v, delta_v
+end
+
 graph_pagerank(graph, d) = graph_pagerank(graph, d, normalize(ones(nv(graph)), 1), 10E-15)
+graph_pagerank(graph, d, ::Val{:delta_data}) = graph_pagerank(graph, d, normalize(ones(nv(graph)), 1), 10E-15, Val(:delta_data))
 
 function pagerank_power_iter_with_mass_re(A, d, e, ϵ)
     rᵢ = e
@@ -26,6 +37,22 @@ function pagerank_power_iter_with_mass_re(A, d, e, ϵ)
         δ = norm(rᵢ₊₁ - rᵢ, 1)
     end
     return rᵢ
+end
+
+function pagerank_power_iter_with_mass_re(A, d, e, ϵ, ::Val{:delta_data})
+    rᵢ = e
+    rᵢ₊₁ = rᵢ
+    δ = Inf
+    delta_v = []
+    while ϵ < δ
+        rᵢ = rᵢ₊₁
+        rᵢ₊₁ = d * A * rᵢ
+        ρ = norm(rᵢ, 1) - norm(rᵢ₊₁, 1)
+        rᵢ₊₁ += ρ * e
+        δ = norm(rᵢ₊₁ - rᵢ, 1)
+        push!(delta_v, δ)
+    end
+    return rᵢ, delta_v
 end
 
 function graph_pagerank_basic(graph, d, e, ϵ)
@@ -47,7 +74,7 @@ function matrix_Â(graph, e)
     n = nv(graph)
     c = zeros(n)
     c[outdegree(graph).==0] .= 1.0
-    return A + e * c'
+    return A + sparse(e) * sparse(c')
 end
 matrix_Â(graph) = matrix_Â(graph, normalize(ones(nv(graph)), 1))
 
